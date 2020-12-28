11cbf47019e4509a4e62b10a688eeb8b3860aee0
Ghazali <mghazk@yahoo.com>
2020-12-28 10:03:23 +0800

    refactor layout update student and invoice models

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,22 +2,7 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Finapp</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
+    @include('layouts.partials.head')
 </head>
 
 <body id="page-top">
@@ -26,7 +11,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        @include('includes.sidebar')
+        @include('layouts.partials.sidebar')
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
@@ -39,7 +24,7 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            @include('includes.contentfooter')
+
             <!-- End of Footer -->
 
         </div>
@@ -53,11 +38,8 @@
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    @include('includes.logoutmodal')
 
-
-    @include('includes.footer')
+    @include('layouts.partials.footer')
 
 </body>
 
